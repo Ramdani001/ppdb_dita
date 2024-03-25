1351570929257db04957cb9d0dc339eb2aa1a667
newbiecodeman <dio.rizki123@gmail.com>
2024-03-26 00:06:51 +0700

    Update Component

---
+++ FrontEnd/component/Navbar.php
@@ -0,0 +1,24 @@
+<style>
+  .bg-body-tertiary {
+    background-color: #343a40 !important;
+    padding: 10px 0; 
+  }
+
+  .navbar-brand {
+    color: rgba(255, 255, 255, 0.8) !important; 
+    transition: color 0.3s ease-in-out; 
+  }
+
+  .navbar-brand:hover {
+    color: #cccccc !important;
+  }
+</style>
+<div>
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Memek
+
+    </a>
+  </div>
+</nav>
+</div>
--- FrontEnd/landingpage/landing.inc.php
+++ FrontEnd/landingpage/landing.inc.php
@@ -1,4 +1,3 @@
 <?php
 
-// Content
-echo "Landing Page";
+include "component/Navbar.php";
