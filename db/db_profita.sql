5315cde5dc1102ffe6abcec8b59c765d4ac0c03c
Rizkan Ramdani <rizkan0990@gmail.com>
2024-06-25 21:42:42 +0700

    Update Db

--- db/db_profita.sql
+++ db/db_profita.sql
@@ -28,9 +28,11 @@ CREATE TABLE IF NOT EXISTS `berkas` (
   `kip` varchar(255) DEFAULT NULL,
   `profile` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_berkas`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2419 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table db_profita.berkas: ~0 rows (approximately)
+-- Dumping data for table db_profita.berkas: ~2 rows (approximately)
+REPLACE INTO `berkas` (`id_berkas`, `kk`, `akta`, `ijazah`, `kip`, `profile`) VALUES
+	(-1, '667ac7afc4f72.png', '66798954d6608.jpg', '', '-', '667ac73c49052.png');
 
 -- Dumping structure for table db_profita.parents
 CREATE TABLE IF NOT EXISTS `parents` (
@@ -39,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `parents` (
   `nik_ayah` varchar(50) DEFAULT NULL,
   `nama_ayah` varchar(255) DEFAULT NULL,
   `lhir_ayah` varchar(80) DEFAULT NULL,
-  `tgl_lhr_ayah` datetime DEFAULT NULL,
+  `tgl_lhr_ayah` date DEFAULT NULL,
   `pendidikan_ayah` varchar(30) DEFAULT NULL,
   `pekerjaan_ayah` varchar(50) DEFAULT NULL,
   `penghasilan_ayah` varchar(255) DEFAULT NULL,
@@ -47,16 +49,16 @@ CREATE TABLE IF NOT EXISTS `parents` (
   `nik_ibu` varchar(60) DEFAULT NULL,
   `nama_ibu` varchar(255) DEFAULT NULL,
   `lhir_ibu` varchar(60) DEFAULT NULL,
-  `tgl_lhr_ibu` datetime DEFAULT NULL,
+  `tgl_lhr_ibu` date DEFAULT NULL,
   `no_ibu` varchar(20) DEFAULT NULL,
   `pendidikan_ibu` varchar(20) DEFAULT NULL,
   `penghasilan_ibu` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`id_parent`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=163 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table db_profita.parents: ~0 rows (approximately)
 REPLACE INTO `parents` (`id_parent`, `id_siswa`, `nik_ayah`, `nama_ayah`, `lhir_ayah`, `tgl_lhr_ayah`, `pendidikan_ayah`, `pekerjaan_ayah`, `penghasilan_ayah`, `status_ayah`, `nik_ibu`, `nama_ibu`, `lhir_ibu`, `tgl_lhr_ibu`, `no_ibu`, `pendidikan_ibu`, `penghasilan_ibu`) VALUES
-	(10, 3692, '345345345345345', 'Robert', 'Bandung', '2024-06-02 00:00:00', 'SMA', 'Pengusaha', '1.000.000 - 2.000.000', 'Hidup', '567342545656', 'Cindy', 'Canada', '2024-06-12 00:00:00', '764656546456', 'S3', '6.000.000 - 10.000.000');
+	(162, 3692, '345345345345345', 'Robert', 'Bandung', '2024-06-25', 'D3', 'Pengusaha', '1.000.000 - 2.000.000', 'Meninggal', '567342545656', 'Cindy', 'Canada', '2024-06-18', '764656546456', 'S3', '1.000.000 - 2.000.000');
 
 -- Dumping structure for table db_profita.person
 CREATE TABLE IF NOT EXISTS `person` (
@@ -78,15 +80,17 @@ CREATE TABLE IF NOT EXISTS `person` (
   `tanggal_lahir` date DEFAULT NULL,
   `rt` varchar(10) DEFAULT NULL,
   `rw` varchar(10) DEFAULT NULL,
+  `id_berkas` int DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id_person`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6913 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table db_profita.person: ~1 rows (approximately)
-REPLACE INTO `person` (`id_person`, `nama`, `no_telp`, `email`, `tipe`, `agama`, `alamat`, `desa`, `kecamatan`, `kab_kota`, `provinsi`, `kode_pos`, `kewarganegaraan`, `tempat_lhir`, `jk`, `tanggal_lahir`, `rt`, `rw`, `created_at`, `updated_at`) VALUES
-	(85, 'Shinta', '', 'shinta@gmail.com', 3, '', '', '', '', '', '', '', '', '', '', '2024-06-08', '', '', '2024-06-07 20:08:28', '2024-06-07 20:08:28'),
-	(192, 'Rizkan Ramdani', '098787867878', 'rizkan@gmail.com', 3, 'Islam', 'JL.Babakan Jati', 'Gumuruh', 'Batununggal', 'Kota Bandung', 'Jawa Barat', '40863', 'Indonesia', 'Bandung', 'Laki-laki', '2024-06-05', '12', '32', '2024-05-25 06:02:52', '2024-06-08 03:51:58');
+-- Dumping data for table db_profita.person: ~3 rows (approximately)
+REPLACE INTO `person` (`id_person`, `nama`, `no_telp`, `email`, `tipe`, `agama`, `alamat`, `desa`, `kecamatan`, `kab_kota`, `provinsi`, `kode_pos`, `kewarganegaraan`, `tempat_lhir`, `jk`, `tanggal_lahir`, `rt`, `rw`, `id_berkas`, `created_at`, `updated_at`) VALUES
+	(6, 'Administrator', '', 'profita.admin@gmail.com', 1, '', '', '', '', '', '', '', '', '', '', '2024-06-25', '', '', 0, '2024-06-25 07:17:32', '2024-06-25 07:17:32'),
+	(85, 'Shinta', '', 'shinta@gmail.com', 3, '', '', '', '', '', '', '', '', '', '', '2024-06-08', '', '', NULL, '2024-06-07 20:08:28', '2024-06-07 20:08:28'),
+	(192, 'Rizkan Ramdani', '098787867878', 'rizkan@gmail.com', 3, 'Islam                                             ', 'Jl. Soekarno Hatta 2039', 'Gumuruh', 'Batununggal', 'Kota Bandung', 'Jawa Barat', '40866', 'Indonesia', 'Bandung', 'Laki-laki', '2024-06-25', '12', '32', -1, '2024-05-25 06:02:52', '2024-06-25 14:13:31');
 
 -- Dumping structure for table db_profita.siswa
 CREATE TABLE IF NOT EXISTS `siswa` (
@@ -110,14 +114,16 @@ CREATE TABLE IF NOT EXISTS `siswa` (
   `jml_saudara` varchar(50) DEFAULT NULL,
   `no_kk` varchar(50) DEFAULT NULL,
   `kepala_keluarga` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  `jurusan` varchar(60) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id_siswa`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4691 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table db_profita.siswa: ~0 rows (approximately)
-REPLACE INTO `siswa` (`id_siswa`, `id_person`, `no_pendaftaran`, `asal_sekolah`, `npsn_sekolah_asal`, `nisn`, `nik`, `biaya_sekolah`, `sd`, `smp`, `kip`, `cita_cita`, `hobi`, `anak_ke`, `transportasi`, `jarak_sekolah`, `waktu_tempuh`, `jml_saudara`, `no_kk`, `kepala_keluarga`, `created_at`, `updated_at`) VALUES
-	(3692, 192, '09808008', 'SMP Bhayangkary', '1234323423423', '34987457495', '0987876567870008', 'Sendiri', 'SDN GUMURUH', 'SMP KEMALA', '-', 'Bisa Terbang', 'Tidur', '1', 'Motor', '> 12Km', '12 Jam', '2', '23425345345213424', 'Robert', '2024-05-25 18:15:10', '2024-06-08 03:51:58');
+REPLACE INTO `siswa` (`id_siswa`, `id_person`, `no_pendaftaran`, `asal_sekolah`, `npsn_sekolah_asal`, `nisn`, `nik`, `biaya_sekolah`, `sd`, `smp`, `kip`, `cita_cita`, `hobi`, `anak_ke`, `transportasi`, `jarak_sekolah`, `waktu_tempuh`, `jml_saudara`, `no_kk`, `kepala_keluarga`, `status`, `jurusan`, `created_at`, `updated_at`) VALUES
+	(3692, 192, '111111', 'SMP Bhayangkary', '1234323423423', '34987457495', '0987876567870008', 'Sendiri', 'SDN GUMURUH', 'SMP KEMALA                                        ', '453423234543', 'Bisa Terbang                                      ', 'Tidur                                             ', '1                                                 ', 'Motor', '> 12Km', '12 Jam', '2                                                 ', '23425345345213424', 'Robert', NULL, 'Akuntansi', '2024-05-25 18:15:10', '2024-06-25 14:13:31');
 
 -- Dumping structure for table db_profita.user
 CREATE TABLE IF NOT EXISTS `user` (
@@ -127,10 +133,11 @@ CREATE TABLE IF NOT EXISTS `user` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=193 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2305 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table db_profita.user: ~0 rows (approximately)
+-- Dumping data for table db_profita.user: ~3 rows (approximately)
 REPLACE INTO `user` (`id_user`, `id_person`, `password`, `created_at`, `updated_at`) VALUES
+	(6, 6, '240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9', '2024-06-25 07:17:32', '2024-06-25 07:17:32'),
 	(85, 85, 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', '2024-06-07 20:08:28', '2024-06-07 20:08:28'),
 	(192, 192, 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', '2024-05-25 06:02:52', '2024-05-25 06:02:52');
 
