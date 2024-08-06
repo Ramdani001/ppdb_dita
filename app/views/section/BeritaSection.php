0192670405b99acd2d87aaab6afe9c235c8e8ff8
Rizkan00 <rizkan0990@gmail.com>
2024-08-06 18:27:38 +0700

    Galeri

--- app/views/section/BeritaSection.php
+++ app/views/section/BeritaSection.php
@@ -5,17 +5,17 @@
     <fieldset class="mt-3">
         <div class="card shadow-md">
             <div class="card-body d-flex justify-content-between flex-wrap gap-2" >
-                    <img src="<?= BASEURL ?>public/assets/img/berita/ber1.jpeg" class="card-img-top" alt="Berita" style="width: 25rem;">
+                    <img src="<?= BASEURL ?>public/assets/img/berita/berita1.jpg" class="card-img-top" alt="Berita" style="width: 25rem;">
                     <div class="" style="width: 60%;">
                         <h2 class="text-2xl" style="color: #373737;">
-                            <b>Juara 1 Se-Provinsi</b> 
+                            <b>Juara Umum dan Juara Medali Emas</b> 
                         </h2>
                         <p style="color: gray;">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore quod, qui asperiores rerum ut voluptatum impedit obcaecati veritatis sunt doloremque autem repellendus ipsa officia facilis praesentium, consectetur ullam iure quos nostrum soluta est a? Et dicta modi nostrum dolores eius nesciunt explicabo illum, quos ipsa vero, omnis quasi esse tenetur assumenda asperiores ab eum facere soluta sed. Neque enim, nihil repudiandae earum numquam accusamus reprehenderit magnam distinctio libero dolor incidunt magni perferendis aliquid, non voluptates fugiat voluptatibus odit et eaque nesciunt deserunt culpa. Ullam dolores earum vel ut facilis possimus quo voluptatibus quis fugit tempore rerum
+                        Assalamualaikum warahmatullahi wabarakatuh Halooo, perkenalkan nama aku Nazmi Mu'aidanissa dari kelas 11 AKL 1 jadi sekarang aku akan berbagi sedikit cerita, aku mulai tertarik ikut lomba itu dari kelas 10, lalu beberapa hari yang lalu aku coba mengikuti 2 lomba nasional yaitu "Nasional Science & English Competition" yang di selenggarakan oleh 'Lembaga Prestasi Pelajar - Liga Olimpiade - HIMA Biologi Institut Teknologi Bandung' dengan mengambil bidang bahasa inggris, dan juga lomba "Indonesian Student Brilliant Olympiad" yang di selenggarakan oleh 'Satu Presentasi - Lembaga Pakat Akademi - HMTI UGM' dengan mengambil bidang sejarah, kedua lomba ini di laksanakan via online menggunakan sistem CBT, dan alhamdulilah di kedua lomba ini aku mendapatkan "gold medal". Sekian yang bisa aku ceritakan, terimakasih. wassalamu'alaikum warahmatullahi wabarakatuh 
                         </p>
-                        <button class="btn btn-primary">
+                        <!-- <button class="btn btn-primary">
                             Baca Selengkapnya
-                        </button>
+                        </button> -->
                     </div>
             </div>
         </div>
