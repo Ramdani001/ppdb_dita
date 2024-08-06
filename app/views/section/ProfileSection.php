ebef8129aeae7a0b74701393f0af574f07d332e4
Rizkan00 <rizkan0990@gmail.com>
2024-08-06 18:28:24 +0700

    s

--- app/views/section/ProfileSection.php
+++ app/views/section/ProfileSection.php
@@ -17,7 +17,7 @@
                 <button onclick="changeProf('s')" id="btnSambutan" type="button" style="z-index: 2; background-color: transparent; padding: 2px 10px; border: none;">Sambutan</button>
             </div>
             <p id="textProfile">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae reiciendis, quod nemo consequatur magnam labore vel minus? Aspernatur blanditiis id repellat vero, aliquam totam quaerat, ipsum voluptate molestiae laborum eligendi nostrum commodi consequatur est illum possimus tempore quod qui. Labore, maxime autem necessitatibus provident expedita consequatur doloribus voluptate ad cum odit, omnis, perspiciatis temporibus asperiores quia architecto quam excepturi nulla magni totam fugiat incidunt. Nam dolore maxime ullam laudantium minus! Dolor nulla, molestiae incidunt ipsum assumenda possimus minus perspiciatis suscipit placeat dolore debitis eum doloremque doloribus. Consequatur sequi consequuntur quae optio, magni aliquam, itaque dicta aperiam eum rem esse mollitia.
+            Pengantar Kepala Sekolah. Sebagai lembaga pendidikan, SMK PROFITA BANDUNG tanggap dengan perkembangan teknologi tersebut. Dengan dukungan SDM/Guru (lihat :Tenaga Pendidik dan lihat : Tenaga Kependidikan) yang di miliki sekolah ini siap untuk berkompetisi dengan sekolah lain dalam pelayanan informasi publik. Teknologi Informasi Web khususnya, menjadi sarana bagi SMK PROFITA BANDUNG untuk memberi pelayanan informasi secara cepat, jelas, dan akuntable. Dari layanan ini pula, sekolah siap menerima saran dari semua pihak yang akhirnya dapat menjawab Kebutuhan masyarakat.
             </p>
             <!-- Yang ini -->
             <!-- <p id="textSambutan" class="d-none h-100">
