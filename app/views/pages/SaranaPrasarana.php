d0c072b7b9b730cf3354940623f3736736664e5e
Rizkan00 <rizkan0990@gmail.com>
2024-08-06 17:41:09 +0700

    Pembaruan Landing Page

--- app/views/pages/SaranaPrasarana.php
+++ app/views/pages/SaranaPrasarana.php
@@ -1,59 +1,134 @@
 <section class="bg-light p-5" style="width: 100%; height: 100%; padding-top: 100px;">
-    <h1 class="mt-5 text-center pb-5">Sarana & Prasarana</h1>
+    <h1 class="mt-5 pb-5">Pic Sarana & Prasarana</h1>
 
     <div class="d-flex flex-wrap justify-content-between gap-4">
         <!-- Card -->
-        <div class="card" style="width: 18rem; cursor: pointer;" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            <img src="<?= BASEURL ?>public/assets/img/guru/guru1.jpg" class="card-img-top" alt="guru">
-            <input type="text" name="idGur" id="igGur">
+        <div class="card" style="width: 14rem; cursor: pointer; height: 16rem;">
+            <img src="<?= BASEURL ?>public/assets/img/sarana/gerbang.jpeg" style="height: 100%; object-fit: cover;" class="card-img-top" alt="guru">
+            <!-- <input type="text" name="idGur" id="igGur"> -->
             <div class="card-body text-center">
                 <div class="card-tittle text-center">
-                    Adelia Vida Paramesti, S.pd.
+                    Gerbang Sekolah
                 </div>
-                <div class="card-text text-secondary">Nip : xxxxxxxxxxxxxxxx</div>
-                <div class="card-text text-secondary">Tahun Masuk : 2020</div>
             </div>
         </div>
         <!-- Card -->
-        <div class="card" style="width: 18rem; cursor: pointer;" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            <img src="<?= BASEURL ?>public/assets/img/guru/guru2.jpg" class="card-img-top" alt="guru">
+        <div class="card" style="width: 14rem; cursor: pointer; height: 16rem;">
+            <img src="<?= BASEURL ?>public/assets/img/sarana/gedung.jpeg" style="height: 100%; object-fit: cover;" class="card-img-top" alt="guru">
+            <!-- <input type="text" name="idGur" id="igGur"> -->
             <div class="card-body text-center">
                 <div class="card-tittle text-center">
-                    Adelia Vida Paramesti, S.pd.
+                    Gedung Sekolah
                 </div>
-                <div class="card-text text-secondary">Nip : xxxxxxxxxxxxxxxx</div>
-                <div class="card-text text-secondary">Tahun Masuk : 2020</div>
             </div>
         </div>
         <!-- Card -->
-        <div class="card" style="width: 18rem; cursor: pointer;" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            <img src="<?= BASEURL ?>public/assets/img/guru/guru3.jpg" class="card-img-top" alt="guru">
+        <div class="card" style="width: 14rem; cursor: pointer; height: 16rem;">
+            <img src="<?= BASEURL ?>public/assets/img/sarana/ruang_kelas.jpeg" style="height: 100%; object-fit: cover;" class="card-img-top" alt="guru">
+            <!-- <input type="text" name="idGur" id="igGur"> -->
             <div class="card-body text-center">
                 <div class="card-tittle text-center">
-                    Adelia Vida Paramesti, S.pd.
+                    Ruang Kelas
                 </div>
-                <div class="card-text text-secondary">Nip : xxxxxxxxxxxxxxxx</div>
-                <div class="card-text text-secondary">Tahun Masuk : 2020</div>
             </div>
         </div>
         <!-- Card -->
-        <div class="card" style="width: 18rem; cursor: pointer;" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                <img src="<?= BASEURL ?>public/assets/img/guru/guru4.jpg" class="card-img-top" alt="guru">
-                <div class="card-body text-center">
-                    <div class="card-tittle text-center">
-                        Adelia Vida Paramesti, S.pd.
-                    </div>
-                    <div class="card-text text-secondary">Nip : xxxxxxxxxxxxxxxx</div>
-                    <div class="card-text text-secondary">Tahun Masuk : 2020</div>
+        <div class="card" style="width: 14rem; cursor: pointer; height: 16rem;">
+            <img src="<?= BASEURL ?>public/assets/img/sarana/perpus.jpg" style="height: 100%; object-fit: cover;" class="card-img-top" alt="guru">
+            <!-- <input type="text" name="idGur" id="igGur"> -->
+            <div class="card-body text-center">
+                <div class="card-tittle text-center">
+                    Perpustakaan
                 </div>
             </div>
         </div>
+        <!-- Card -->
+        <div class="card" style="width: 14rem; cursor: pointer; height: 16rem;">
+            <img src="<?= BASEURL ?>public/assets/img/sarana/lapangan sekolah.jpg" style="height: 100%; object-fit: cover;" class="card-img-top" alt="guru">
+            <!-- <input type="text" name="idGur" id="igGur"> -->
+            <div class="card-body text-center">
+                <div class="card-tittle text-center">
+                    Lapangan Sekolah
+                </div>
+            </div>
+        </div>
+        <!-- Card -->
+        <div class="card" style="width: 14rem; cursor: pointer; height: 16rem;">
+            <img src="<?= BASEURL ?>public/assets/img/sarana/rakpiala.jpeg" style="height: 100%; object-fit: cover;" class="card-img-top" alt="guru">
+            <!-- <input type="text" name="idGur" id="igGur"> -->
+            <div class="card-body text-center">
+                <div class="card-tittle text-center">
+                    Rak Piala
+                </div>
+            </div>
+        </div>
+        <!-- Card -->
+        <div class="card" style="width: 14rem; cursor: pointer; height: 16rem;">
+            <img src="<?= BASEURL ?>public/assets/img/sarana/resepsionis.jpeg" style="height: 100%; object-fit: cover;" class="card-img-top" alt="guru">
+            <!-- <input type="text" name="idGur" id="igGur"> -->
+            <div class="card-body text-center">
+                <div class="card-tittle text-center">
+                    Resepsionis
+                </div>
+            </div>
+        </div>
+        <!-- Card -->
+        <div class="card" style="width: 14rem; cursor: pointer; height: 16rem;">
+            <img src="<?= BASEURL ?>public/assets/img/sarana/ruang_guru.jpeg" style="height: 100%; object-fit: cover;" class="card-img-top" alt="guru">
+            <!-- <input type="text" name="idGur" id="igGur"> -->
+            <div class="card-body text-center">
+                <div class="card-tittle text-center">
+                    Ruang Guru
+                </div>
+            </div>
+        </div>
+        <!-- Card -->
+        <div class="card" style="width: 14rem; cursor: pointer; height: 16rem;">
+            <img src="<?= BASEURL ?>public/assets/img/sarana/lab_komputer.jpeg" style="height: 100%; object-fit: cover;" class="card-img-top" alt="guru">
+            <!-- <input type="text" name="idGur" id="igGur"> -->
+            <div class="card-body text-center">
+                <div class="card-tittle text-center">
+                    Lab Komputer
+                </div>
+            </div>
+        </div>
+        <!-- Card -->
+        <div class="card" style="width: 14rem; cursor: pointer; height: 16rem;">
+            <img src="<?= BASEURL ?>public/assets/img/sarana/ruang_tu.jpg" style="height: 100%; object-fit: cover;" class="card-img-top" alt="guru">
+            <!-- <input type="text" name="idGur" id="igGur"> -->
+            <div class="card-body text-center">
+                <div class="card-tittle text-center">
+                    Ruang T.U
+                </div>
+            </div>
+        </div>
+        <!-- Card -->
+        <div class="card" style="width: 14rem; cursor: pointer; height: 16rem;">
+            <img src="<?= BASEURL ?>public/assets/img/sarana/ruang_meeting.jpeg" style="height: 100%; object-fit: cover;" class="card-img-top" alt="guru">
+            <!-- <input type="text" name="idGur" id="igGur"> -->
+            <div class="card-body text-center">
+                <div class="card-tittle text-center">
+                    Ruang Meeting
+                </div>
+            </div>
+        </div>
+        <!-- Card -->
+        <div class="card" style="width: 14rem; cursor: pointer; height: 16rem;">
+            <img src="<?= BASEURL ?>public/assets/img/sarana/masjid.jpeg" style="height: 100%; object-fit: cover;" class="card-img-top" alt="guru">
+            <!-- <input type="text" name="idGur" id="igGur"> -->
+            <div class="card-body text-center">
+                <div class="card-tittle text-center">
+                    Masjid
+                </div>
+            </div>
+        </div>
+
 
 <!-- ================================================ -->
     </div>
 
     <!-- Pagination -->
-    <div class="pt-5">
+    <!-- <div class="pt-5">
         <nav aria-label="Page navigation example">
             <ul class="pagination">
                 <li class="page-item"><a class="page-link text-dark" href="#">Previous</a></li>
@@ -63,7 +138,7 @@
                 <li class="page-item"><a class="page-link text-dark" href="#">Next</a></li>
             </ul>
         </nav>
-    </div>
+    </div> -->
     <!-- Pagination -->
 </section>
 
