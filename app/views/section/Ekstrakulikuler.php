047ef83015c4d3d432755cc0d3c6d2a97b06e217
Rizkan00 <rizkan0990@gmail.com>
2024-08-07 19:20:54 +0700

    Perubahan Front End

--- app/views/section/Ekstrakulikuler.php
+++ app/views/section/Ekstrakulikuler.php
@@ -5,9 +5,9 @@
     <fieldset class="d-flex flex-wrap justify-content-evenly gap-3">
         <!-- Card -->
         <div class="card eks shadow-md" style="width: 25rem; cursor: pointer;">
-          <img src="<?= BASEURL ?>public/assets/img/pmr.png" class="card-img-top" alt="...">
+          <img src="<?= BASEURL ?>public/assets/img/padus.jpg" class="card-img-top" alt="...">
           <div id="cardBody-eks" class="card-body position-absolute bottom-0">
-            <h5 class="card-title">PMR</h5>
+            <h5 class="card-title">Paduan Suara</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           </div>
         </div>
@@ -24,9 +24,9 @@
         
         <!-- Card -->
         <div class="card eks shadow-md" style="width: 25rem; cursor: pointer;">
-          <img src="<?= BASEURL ?>public/assets/img/pmr.png" class="card-img-top" alt="...">
+          <img src="<?= BASEURL ?>public/assets/img/irma.jpg" class="card-img-top" alt="...">
           <div id="cardBody-eks" class="card-body position-absolute bottom-0">
-            <h5 class="card-title">PMR</h5>
+            <h5 class="card-title">IRMA</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           </div>
         </div>
--- app/views/section/GaleriSection.php
+++ app/views/section/GaleriSection.php
@@ -8,7 +8,6 @@
           <img src="<?= BASEURL ?>public/assets/img/gal1.jpg" class="card-img-top" alt="Galeri" style="height: 17rem; object-fit: cover; border-radius: 6px;" >
           <div id="cardBody-gal" class="card-body position-absolute bottom-0 w-100 text-center text-light" style="background-color: #00000080;">
             <h5 class="card-title">PMR</h5>
-            <p class="card-text">12 Photo</p>
           </div>
         </div>
         <!-- Card --> 
@@ -16,8 +15,7 @@
          <div class="card shadow-md" style="width: 17rem; height: 17rem; cursor: pointer; border-radius: 6px; background-color: transparent;">
           <img src="<?= BASEURL ?>public/assets/img/galeri/gal2.jpg" class="card-img-top" alt="Galeri" style="height: 17rem; object-fit: cover; border-radius: 6px;" >
           <div id="cardBody-gal" class="card-body position-absolute bottom-0 w-100 text-center text-light" style="background-color: #00000080;">
-            <h5 class="card-title">PMR</h5>
-            <p class="card-text">12 Photo</p>
+            <h5 class="card-title">Expo SPW SMK Profita Bandung</h5>
           </div>
         </div>
         <!-- Card -->
@@ -25,8 +23,7 @@
          <div class="card shadow-md" style="width: 17rem; height: 17rem; cursor: pointer; border-radius: 6px; background-color: transparent;">
           <img src="<?= BASEURL ?>public/assets/img/galeri/gal3.jpeg" class="card-img-top" alt="Galeri" style="height: 17rem; object-fit: cover; border-radius: 6px;" >
           <div id="cardBody-gal" class="card-body position-absolute bottom-0 w-100 text-center text-light" style="background-color: #00000080;">
-            <h5 class="card-title">PMR</h5>
-            <p class="card-text">12 Photo</p>
+            <h5 class="card-title">Maulid Nabi Muhammad SAW</h5>
           </div>
         </div>
         <!-- Card -->
@@ -34,8 +31,7 @@
          <div class="card shadow-md" style="width: 17rem; height: 17rem; cursor: pointer; border-radius: 6px; background-color: transparent;">
           <img src="<?= BASEURL ?>public/assets/img/galeri/gal4.jpg" class="card-img-top" alt="Galeri" style="height: 17rem; object-fit: cover; border-radius: 6px;" >
           <div id="cardBody-gal" class="card-body position-absolute bottom-0 w-100 text-center text-light" style="background-color: #00000080;">
-            <h5 class="card-title">PMR</h5>
-            <p class="card-text">12 Photo</p>
+            <h5 class="card-title">P% Kebekerjaan</h5>
           </div>
         </div>
         <!-- Card -->
@@ -43,8 +39,7 @@
          <div class="card shadow-md" style="width: 17rem; height: 17rem; cursor: pointer; border-radius: 6px; background-color: transparent;">
           <img src="<?= BASEURL ?>public/assets/img/galeri/gal5.jpg" class="card-img-top" alt="Galeri" style="height: 17rem; object-fit: cover; border-radius: 6px;" >
           <div id="cardBody-gal" class="card-body position-absolute bottom-0 w-100 text-center text-light" style="background-color: #00000080;">
-            <h5 class="card-title">PMR</h5>
-            <p class="card-text">12 Photo</p>
+            <h5 class="card-title">Pembekalan PKL Pemasaran</h5>
           </div>
         </div>
         <!-- Card -->
@@ -52,8 +47,7 @@
          <div class="card shadow-md" style="width: 17rem; height: 17rem; cursor: pointer; border-radius: 6px; background-color: transparent;">
           <img src="<?= BASEURL ?>public/assets/img/galeri/gal6.jpg" class="card-img-top" alt="Galeri" style="height: 17rem; object-fit: cover; border-radius: 6px;" >
           <div id="cardBody-gal" class="card-body position-absolute bottom-0 w-100 text-center text-light" style="background-color: #00000080;">
-            <h5 class="card-title">PMR</h5>
-            <p class="card-text">12 Photo</p>
+            <h5 class="card-title">40 Karakter Baik</h5>
           </div>
         </div>
         <!-- Card -->
