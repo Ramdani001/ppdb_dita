4f194fd200b3fe81cee4437483f8eb27154afc26
Rizkan00 <rizkan0990@gmail.com>
2024-08-12 08:22:30 +0700

    Messages

--- app/views/ppdb/berkas.php
+++ app/views/ppdb/berkas.php
@@ -62,24 +62,27 @@
   <nav class="header-nav ms-auto">
     <ul class="d-flex align-items-center">
 
-    <?php if((int)$siswa['st'] != 0) {  
-        echo '
-            <div class="me-3 fs-4" style="color: rgb(33, 37, 41);">
-            <div style="width: 15px; height: 15px; background-color: blue; position: absolute; border-radius: 100%; margin-top: 2px; margin-left: -5px;"></div>
-              <a href="'.BASEURL.'/AdminController/messages/'.$person['id_person'].'" target="_blank" style="text-decoration: none; color: blue;">
-              <i class="bi bi-envelope"></i> 
-              </a>
-            </div>
-        ';
-      }else{
-        echo '
-            <div class="me-3 fs-4" style="color: rgb(33, 37, 41);">
-              <button type="button" style="background: none; border: none;" id="pesan_button">
-                <i class="bi bi-envelope"></i> 
-              </button>
-            </div>
-        ';
-      }
+    <?php
+        if($siswa){
+          if((int)$siswa['st'] != 0) {
+            echo '
+                <div class="me-3 fs-4" style="color: rgb(33, 37, 41);">
+                <div style="width: 15px; height: 15px; background-color: blue; position: absolute; border-radius: 100%; margin-top: 2px; margin-left: -5px;"></div>
+                  <a href="'.BASEURL.'/AdminController/messages/'.$person['id_person'].'" target="_blank" style="text-decoration: none; color: blue;">
+                  <i class="bi bi-envelope"></i> 
+                  </a>
+                </div>
+            ';
+          }else{
+            echo '
+                <div class="me-3 fs-4" style="color: rgb(33, 37, 41);">
+                  <button type="button" style="background: none; border: none;" id="pesan_button">
+                    <i class="bi bi-envelope"></i> 
+                  </button>
+                </div>
+            ';
+          }
+        }
       ?>
 
       <li class="nav-item d-block d-lg-none">
