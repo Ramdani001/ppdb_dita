3029ca264cb58ff38d826e269fcbf4dfbf201ff0
Rizkan00 <rizkan0990@gmail.com>
2024-08-24 20:35:47 +0700

    d

--- app/init.php
+++ app/init.php
@@ -3,7 +3,7 @@
 
 require_once "config/config.php";
 
-require_once("".BASEURL."core/App.php");
-require_once ("".BASEURL."core/Controller.php");
-require_once ("".BASEURL."core/Database.php");
-require_once ("".BASEURL."core/Flasher.php");
+require_once("localhost/SMK_PROFITA/core/App.php");
+require_once ("localhost/SMK_PROFITA/core/Controller.php");
+require_once ("localhost/SMK_PROFITA/core/Database.php");
+require_once ("localhost/SMK_PROFITA/core/Flasher.php");
