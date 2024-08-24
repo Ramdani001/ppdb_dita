202d1e4e4eb257b6b4a082f2896a2cf45f7c340c
Rizkan00 <rizkan0990@gmail.com>
2024-08-24 21:21:46 +0700

    s

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,13 +1,13 @@
 <?php
-
+require_once __DIR__ . '/vendor/autoload.php';
 class Controller
 {
     public function view($view, $data = []){
-        require_once BASEURL . 'app/views/' . $view . '.php';
+        require_once BASEURL.'app/views/' . $view . '.php';
     }
 
     public function model($model){
-        require_once BASEURL . 'app/models/' . $model . '.php';
+        require_once BASEURL.'app/models/' . $model . '.php';
         return new $model;
     }
 
