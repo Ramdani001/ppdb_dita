817a8e50645e766e36e6cd141200eb3f5a0dab0e
Rizkan00 <rizkan0990@gmail.com>
2024-08-24 20:39:56 +0700

    a

--- app/init.php
+++ app/init.php
@@ -3,7 +3,7 @@
 
 require_once "config/config.php";
 
-require_once("http://203.175.10.169/SMK_PROFITA/core/App.php");
-require_once ("http://203.175.10.169/SMK_PROFITA/core/Controller.php");
-require_once ("http://203.175.10.169/SMK_PROFITA/core/Database.php");
-require_once ("http://203.175.10.169/SMK_PROFITA/core/Flasher.php");
+require_once("./core/App.php");
+require_once ("./core/Controller.php");
+require_once ("./core/Database.php");
+require_once ("./core/Flasher.php");
